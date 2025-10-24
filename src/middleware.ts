59af67deaa88af70800e7c64760af964ac607a8f
rudradas05 <dasrudra.stu@gmail.com>
2025-10-25 01:48:51 +0530

    verified user bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
-  // "/dashboard(.*)",
+  "/dashboard(.*)",
   "/verify(.*)",
 ]);
 
